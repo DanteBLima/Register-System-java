e833925777cc63b4b8d3c2b0107d9ec51e7ef331
Dante Borges Lima <dante.borges@dinfor.uniceub>
2024-08-13 16:06:35 -0300

    Updating

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import domain.FormReader;
+import domain.RegisterUser;
+import domain.User;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,6 +8,7 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,6 +19,12 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Scanner scanner = new Scanner(System.in);
+        User user = new User();
+        RegisterUser registerUser = new RegisterUser(user, scanner);
+        System.out.println("Choose an option ");
+        System.out.println(COLOCA A FUNCAO GETFORMQUESTIONS AQUI);
 
     }
 }
